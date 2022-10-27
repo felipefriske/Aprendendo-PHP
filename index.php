508047bc15045efa6e1709a0be707a9c81068a79
felipefriske <116766407+felipefriske@users.noreply.github.com>
2022-10-26 22:52:00 -0300

    tentandop gerar conflito

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-      echo "meu segundo teste no php, meu deus quanto erro ate chegar aqui"
+      echo "meu segundo teste no php, meu deus quanto erro ate chegar aqui 1";
+
+      echo "tentando gerar conflito com meu repositorio do pc";
 
 ?>
